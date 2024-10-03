d2a719919afabf9979fcdcadf76eaec6b62c5f6b
sahiltambe18 <sahiltambe81922@gmail.com>
2024-10-03 23:52:37 +0530

    ui changes

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -7,33 +7,38 @@ import { Auth } from './pages/auth';
 import { FinancialRecordsProvider } from './contexts/financial-record-context';
 import { SignedIn, useAuth, UserButton } from '@clerk/clerk-react';
 
-
+import img from './images/tracker.jpg';
 
 function App() {
 
-    const {isSignedIn} =  useAuth()
-    
-    
+  const { isSignedIn } = useAuth()
+
+
   return (
-    
+
 
     <Router>
       <div className="app-container">
         <div className='navbar'>
-         
+          <p className='title'>
+            Funds Tracker
+          </p>
           <SignedIn>
-                <UserButton showName afterSignOutUrl="/dashboard"/>
-                
-
-            </SignedIn>
+            <UserButton showName afterSignOutUrl="/dashboard" />
+          </SignedIn>
         </div>
         <Routes>
           <Route path="/dashboard" element={  // all the componest in dashboard will have acces to record provider componenets
             <FinancialRecordsProvider>
-              
-              { isSignedIn ? <Dashboard /> : <Navigate to="/" /> }
+
+              {isSignedIn ? <Dashboard /> : <Navigate to="/" />}
             </FinancialRecordsProvider>} />
-          <Route path="/"  element={<Auth />} />
+          <Route path="/" element={
+            <div style={{ backgroundImage: `url(${img})` }} className='app-container' >
+              <Auth />
+            </div>
+
+          } />
         </Routes>
       </div>
     </Router>
--- client/src/pages/dashboard/index.tsx
+++ client/src/pages/dashboard/index.tsx
@@ -4,28 +4,29 @@ import { FinancialRecordList } from "./financial-record-list";
 import { useFinancialRecords } from "../../contexts/financial-record-context";
 import { useMemo } from "react";
 
-export const Dashboard  = () =>
-{
+export const Dashboard = () => {
 
     const { user } = useUser();
     const { records } = useFinancialRecords();
 
-    const totalMonthly = useMemo(() =>
-    {
+    const totalMonthly = useMemo(() => {
         let totalAmount = 0;
-        records.forEach((record) =>
-        {
+        records.forEach((record) => {
             totalAmount += record.amount;
         });
 
         return totalAmount;
     }, [records]);
-    return(
+    return (
         <div className="dashboard-container">
-            <h1> Welcome { user?.firstName}! Here are your finances:</h1>
-            <FinancialRecordForm />
-            <div>Total Monthly: ${totalMonthly}</div>
-            <FinancialRecordList />
+            <h1> Welcome {user?.firstName}! Here are your finances:</h1>
+            <div className="dashboard" >
+                <FinancialRecordForm />
+                <div>
+                    <div>Total Monthly: ${totalMonthly}</div>
+                    <FinancialRecordList />
+                </div>
+            </div>
         </div>
 
     )
